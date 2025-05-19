1663e07e43532163890a1746c95a2c574e839ec7
Randal Goss <randal.s.goss@usace.army.mil>
2025-05-19 11:44:32 -0400

    added more unit tests

--- src/test/java/usace/cc/plugin/api/IOManagerTest.java
+++ src/test/java/usace/cc/plugin/api/IOManagerTest.java
@@ -2,9 +2,17 @@ package usace.cc.plugin.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
+
+
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +20,8 @@ import org.junit.jupiter.api.Test;
 
 import com.amazonaws.services.s3.AmazonS3;
 
+import usace.cc.plugin.api.DataStore.DataStoreException;
+import usace.cc.plugin.api.IOManager.InvalidDataSourceException;
 import usace.cc.plugin.api.IOManager.InvalidDataStoreException;
 
 public class IOManagerTest {
@@ -37,5 +47,110 @@ public class IOManagerTest {
             Assertions.fail("Unable to load S3STORE");
         }
     }
+
+    @Test
+    public void testGetDataSource() throws InvalidDataSourceException {
+        var inputName = "image1";
+        GetDataSourceInput gdsi = new GetDataSourceInput(inputName, DataSourceIOType.INPUT);
+        Optional<DataSource> dataSourceOpt = payload.getDataSource(gdsi);
+        assertTrue(dataSourceOpt.isPresent());
+        var dataSource = dataSourceOpt.get();
+        assertEquals(dataSource.getName(), inputName);
+    }
+
+    @Test
+    public void testGetDataSourceInput() throws InvalidDataSourceException {
+        var inputName = "image1";
+        Optional<DataSource> dataSourceOpt = payload.getInputDataSource(inputName);
+        assertTrue(dataSourceOpt.isPresent());
+        var dataSource = dataSourceOpt.get();
+        assertEquals(dataSource.getName(), inputName);
+    }
+
+    @Test
+    public void testGetDataSourceOutput() throws InvalidDataSourceException {
+        var inputName = "test-out1";
+        Optional<DataSource> dataSourceOpt = payload.getOutputDataSource(inputName);
+        assertTrue(dataSourceOpt.isPresent());
+        var dataSource = dataSourceOpt.get();
+        assertEquals(dataSource.getName(), inputName);
+    }
+
+    @Test
+    public void testGetInputStream() throws IOException, InvalidDataSourceException, DataStoreException {
+        var inputName = "text1";
+        var expectedResult="hello world";
+        Optional<DataSource> dataSourceOpt = payload.getInputDataSource(inputName);
+        assertTrue(dataSourceOpt.isPresent());
+        var dataSource = dataSourceOpt.get();
+        InputStream is = payload.getInputStream(dataSource, "default");
+        var result = isToString(is);
+        assertEquals(result,expectedResult);
+    }
+
+    @Test
+    public void testCopyFileToLocal() throws IOException, InvalidDataSourceException, DataStoreException {
+        String localPath = "/data/testfile";
+        int fileSize = 146050;
+        payload.copyFileToLocal("image1", "default", localPath);
+        File file = new File(localPath);
+        assertTrue(file.exists());
+        Path path = Paths.get(localPath);
+        var bytes = Files.readAllBytes(path);
+        assertEquals(bytes.length, fileSize);
+    }
+
+    @Test
+    public void testPut() throws IOException, InvalidDataSourceException, DataStoreException, InvalidDataStoreException {
+        byte[] data = "mockData".getBytes();
+        PutObjectOutput putObjectOutput = payload.put(data, "test-out2", "default", null);
+        var transferredData = payload.get("test-in1", "default", null);
+        var md5hash = md5(transferredData);
+        //@TODO review filecode vs content hash in the PutObjectOutput.
+        //byte[] decodedBytes = Base64.getDecoder().decode(putObjectOutput.getHash());
+        //String contentHash = new String(decodedBytes);
+        assertEquals(putObjectOutput.getFilecode(), md5hash);
+        //assertEquals(contentHash, md5hash);
+    }
+
+    @Test
+    public void testCopyFileToRemote() throws IOException, InvalidDataSourceException, DataStoreException, InvalidDataStoreException {
+        String localPath = "/data/testfile";
+        int fileSize = 146050;
+        payload.copyFileToRemote("test-out1", "default", localPath);
+        var response = payload.get("test4","default",null);
+        assertEquals(response.length,fileSize);
+    }
+
+    @Test
+    public void testGetStoreSession() throws InvalidDataStoreException {
+        Optional<FileStore> session = payload.getStoreSession("S3STORE");
+        assertTrue(session.isPresent());
+    }
+
+
+    private static String isToString(InputStream inputStream) throws IOException {
+        var bytes = inputStream.readAllBytes(); 
+        return new String(bytes);
+    }
+
+    public static String md5(byte[] input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input);
+
+            // Convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 algorithm not available", e);
+        }
+    }
+
+
     
 }
--- src/test/java/usace/cc/plugin/api/PluginManagerTest.java
+++ src/test/java/usace/cc/plugin/api/PluginManagerTest.java
@@ -1,35 +1,12 @@
 package usace.cc.plugin.api;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-
 import usace.cc.plugin.api.IOManager.InvalidDataStoreException;
-import usace.cc.plugin.api.eventstore.EventStore.ArrayAttribute;
-import usace.cc.plugin.api.eventstore.EventStore.ArrayDimension;
-import usace.cc.plugin.api.eventstore.EventStore.ArrayType;
-import usace.cc.plugin.api.eventstore.EventStore.AttrType;
-import usace.cc.plugin.api.eventstore.EventStore.CreateArrayInput;
-import usace.cc.plugin.api.eventstore.EventStore.DimensionType;
-import usace.cc.plugin.api.eventstore.EventStore.GetArrayInput;
-import usace.cc.plugin.api.eventstore.EventStore.LayoutOrder;
-import usace.cc.plugin.api.eventstore.EventStore.PutArrayBuffer;
-import usace.cc.plugin.api.eventstore.EventStore.PutArrayInput;
-import usace.cc.plugin.api.eventstore.tiledb.TileDbEventStore;
 
-
-/*
- * getpayload
- * parameterSubtitute
- * pathSubstitute
- * 
- */
 
 public class PluginManagerTest {
 
@@ -48,186 +25,198 @@ public class PluginManagerTest {
         assertEquals(expectedResult, pm.getEventIdentifier());
     }
 
-
-    @Test 
-    void pluginManagerTest() throws Exception{
-
-        PluginManager pm = PluginManager.getInstance();
-
-        Payload payload = pm.getPayload();
-
-        var payloadAttributes = payload.getAttributes();
-
-        //attributes function like a java map of <String,Object>
-        for(Entry<String,Object> entry :payloadAttributes.entrySet()){
-            System.out.println(String.format("%s::%s",entry.getKey(),entry.getValue()));
+    @Test
+    public void testSubstitutePathVariables() {
+        var dsOpt = payload.getInputDataSource("test3");
+        if(dsOpt.isPresent()){
+            var ds = dsOpt.get();
+            var result = ds.getPaths().get("default");
+            var expectedResult = "cc_store/test/987654/hwout.txt";
+            assertEquals(result, expectedResult);
+        } else {
+            Assertions.fail("missing data source");
         }
+    }
 
-        //to get a single value as a specific type
-        Optional<Double> testNumberOpt = payloadAttributes.get("testnumber");
-        if (testNumberOpt.isPresent()){
-            System.out.println(testNumberOpt.get());
-        }
+    // @Test 
+    // void pluginManagerTest() throws Exception{
 
-        Optional<List<Integer>> testArrayOpt = payloadAttributes.get("testarray");
-        if (testArrayOpt.isPresent()){
-            List<Integer> testArrayInt = testArrayOpt.get();
-            System.out.println(testArrayInt);
-        }
+    //     PluginManager pm = PluginManager.getInstance();
 
-        Optional<String> testStringArrayOpt = payloadAttributes.get("teststringarray");
-        if (testStringArrayOpt.isPresent()){
-            var testArrayString = testStringArrayOpt.get();
-            System.out.println(testArrayString);
-        }
+    //     Payload payload = pm.getPayload();
 
-        //You shouldn't need to access the underlying map, however it is accessible:
-        Map<String,Object> attributeMap = payloadAttributes.getAttributes();
-        System.out.println(attributeMap);
+    //     var payloadAttributes = payload.getAttributes();
+
+    //     //attributes function like a java map of <String,Object>
+    //     for(Entry<String,Object> entry :payloadAttributes.entrySet()){
+    //         System.out.println(String.format("%s::%s",entry.getKey(),entry.getValue()));
+    //     }
+
+    //     //to get a single value as a specific type
+    //     Optional<Double> testNumberOpt = payloadAttributes.get("testnumber");
+    //     if (testNumberOpt.isPresent()){
+    //         System.out.println(testNumberOpt.get());
+    //     }
+
+    //     Optional<List<Integer>> testArrayOpt = payloadAttributes.get("testarray");
+    //     if (testArrayOpt.isPresent()){
+    //         List<Integer> testArrayInt = testArrayOpt.get();
+    //         System.out.println(testArrayInt);
+    //     }
+
+    //     Optional<String> testStringArrayOpt = payloadAttributes.get("teststringarray");
+    //     if (testStringArrayOpt.isPresent()){
+    //         var testArrayString = testStringArrayOpt.get();
+    //         System.out.println(testArrayString);
+    //     }
+
+    //     //You shouldn't need to access the underlying map, however it is accessible:
+    //     Map<String,Object> attributeMap = payloadAttributes.getAttributes();
+    //     System.out.println(attributeMap);
 
 
-        //store functions
-        //get all stores
-        DataStore[] stores = payload.getStores();
-        // for (DataStore store:stores){
-        //     System.out.println(store.getName());
-        //     if ("EVENT_STORE".equals(store.getName())){
-        //         var tdb = new TileDbEventStore();
-        //         try{
-        //             tdb.connect(store);
-        //         } catch(Exception ex){
-        //             ex.printStackTrace();
-        //         }
-        //     }
+    //     //store functions
+    //     //get all stores
+    //     DataStore[] stores = payload.getStores();
+    //     // for (DataStore store:stores){
+    //     //     System.out.println(store.getName());
+    //     //     if ("EVENT_STORE".equals(store.getName())){
+    //     //         var tdb = new TileDbEventStore();
+    //     //         try{
+    //     //             tdb.connect(store);
+    //     //         } catch(Exception ex){
+    //     //             ex.printStackTrace();
+    //     //         }
+    //     //     }
             
-        // }
+    //     // }
 
        
-        //get a single store by name
-        Optional<DataStore> storeOpt2 = payload.getStore(stores[0].getName());
-        if(storeOpt2.isPresent()){
-            System.out.println(storeOpt2.get().getName());
-        }
+    //     //get a single store by name
+    //     Optional<DataStore> storeOpt2 = payload.getStore(stores[0].getName());
+    //     if(storeOpt2.isPresent()){
+    //         System.out.println(storeOpt2.get().getName());
+    //     }
 
-        //source functions
-        //get inpout data sources
-        DataSource[] inputSources =  payload.getInputs();
-        for (DataSource source: inputSources){
-            System.out.println(source.getName());
-        }
+    //     //source functions
+    //     //get inpout data sources
+    //     DataSource[] inputSources =  payload.getInputs();
+    //     for (DataSource source: inputSources){
+    //         System.out.println(source.getName());
+    //     }
 
-        //get a single source by name
-        Optional<DataSource> sourceOpt = payload.getInputDataSource(inputSources[0].getName());
-        if(sourceOpt.isPresent()){
-            System.out.println(sourceOpt.get().getName());
-        }
+    //     //get a single source by name
+    //     Optional<DataSource> sourceOpt = payload.getInputDataSource(inputSources[0].getName());
+    //     if(sourceOpt.isPresent()){
+    //         System.out.println(sourceOpt.get().getName());
+    //     }
 
-        //Actions
-        Action[] actions = payload.getActions();
-        for (Action action:actions){
-            System.out.println(action.getType());
-            System.out.println(action.getDescription());
+    //     //Actions
+    //     Action[] actions = payload.getActions();
+    //     for (Action action:actions){
+    //         System.out.println(action.getType());
+    //         System.out.println(action.getDescription());
 
-            //actions have private attributes, stores, inputs, and outputs 
-            var params = action.getAttributes();
-            Optional<String> actionParamOpt = params.get("seed_store_type");
-            if (actionParamOpt.isPresent()){
-                System.out.println(actionParamOpt.get());
-            }
+    //         //actions have private attributes, stores, inputs, and outputs 
+    //         var params = action.getAttributes();
+    //         Optional<String> actionParamOpt = params.get("seed_store_type");
+    //         if (actionParamOpt.isPresent()){
+    //             System.out.println(actionParamOpt.get());
+    //         }
 
-            //attribute searches will also search into the parent (i.e. payload) attributes
-            Optional<String> parentParamOpt = params.get("scenario");
-            if (parentParamOpt.isPresent()){
-                System.out.println(parentParamOpt.get());
-            }
+    //         //attribute searches will also search into the parent (i.e. payload) attributes
+    //         Optional<String> parentParamOpt = params.get("scenario");
+    //         if (parentParamOpt.isPresent()){
+    //             System.out.println(parentParamOpt.get());
+    //         }
 
-        }
-        ///////////////
+    //     }
+    //     ///////////////
 
-        //////////metadata
-        Optional<TileDbEventStore> tiledbStoreOpt = payload.getStoreSession("EVENT_STORE");
-        if (tiledbStoreOpt.isPresent()){
-            var store = tiledbStoreOpt.get();
-            store.putMetadata("TEST99", 1234.9876);
-            Double[] val = store.getMetadata("TEST99", Double[].class);
-            System.out.println(val);
-            Double val1 = store.getMetadata("TEST99", Double.class);
-            System.out.println(val1);
+    //     //////////metadata
+    //     Optional<TileDbEventStore> tiledbStoreOpt = payload.getStoreSession("EVENT_STORE");
+    //     if (tiledbStoreOpt.isPresent()){
+    //         var store = tiledbStoreOpt.get();
+    //         store.putMetadata("TEST99", 1234.9876);
+    //         Double[] val = store.getMetadata("TEST99", Double[].class);
+    //         System.out.println(val);
+    //         Double val1 = store.getMetadata("TEST99", Double.class);
+    //         System.out.println(val1);
 
             
 
-            var attrs = new ArrayAttribute[]{
-                new ArrayAttribute("a1", AttrType.ATTR_FLOAT32),
-                new ArrayAttribute("a2", AttrType.ATTR_INT32),
-                new ArrayAttribute("a3", AttrType.ATTR_STRING)
-            };
+    //         var attrs = new ArrayAttribute[]{
+    //             new ArrayAttribute("a1", AttrType.ATTR_FLOAT32),
+    //             new ArrayAttribute("a2", AttrType.ATTR_INT32),
+    //             new ArrayAttribute("a3", AttrType.ATTR_STRING)
+    //         };
 
-            var dims = new ArrayDimension[]{
-                new ArrayDimension("Y", DimensionType.DIMENSION_INT, new long[]{1,10},5)
-            };
+    //         var dims = new ArrayDimension[]{
+    //             new ArrayDimension("Y", DimensionType.DIMENSION_INT, new long[]{1,10},5)
+    //         };
 
-            ///////////////////////////////////
-            CreateArrayInput cainput = new CreateArrayInput();
-            cainput.arrayPath="/array1";
-            cainput.attributes = attrs;
-            cainput.dimensions = dims;
-            cainput.cellLayout=LayoutOrder.ROWMAJOR;
-            cainput.tileLayout=LayoutOrder.COLMAJOR;
-            cainput.arrayType=ArrayType.ARRAY_DENSE;
+    //         ///////////////////////////////////
+    //         CreateArrayInput cainput = new CreateArrayInput();
+    //         cainput.arrayPath="/array1";
+    //         cainput.attributes = attrs;
+    //         cainput.dimensions = dims;
+    //         cainput.cellLayout=LayoutOrder.ROWMAJOR;
+    //         cainput.tileLayout=LayoutOrder.COLMAJOR;
+    //         cainput.arrayType=ArrayType.ARRAY_DENSE;
 
-            try{
-                store.createArray(cainput);
-            } catch(Exception ex){
-                ex.printStackTrace();
-            }
+    //         try{
+    //             store.createArray(cainput);
+    //         } catch(Exception ex){
+    //             ex.printStackTrace();
+    //         }
 
-            //////////////////////////////
-            PutArrayInput putInput = new PutArrayInput();
-            putInput.arrayPath="/array1";
-            putInput.arrayType=ArrayType.ARRAY_DENSE;
-            putInput.putLayout=LayoutOrder.ROWMAJOR;
-            putInput.bufferRange = new long[]{3,6};
-            var buffer1 = new PutArrayBuffer("a1",new float[]{1.1f, 2.2f, 3.3f, 4.4f});
-            var buffer2 = new PutArrayBuffer("a2", new int[]{9,8,7,5});
-            var buffer3 = new PutArrayBuffer("a3", "thisisthestring");
-            buffer3.offsets = new long[]{0,4,6,9};
-            putInput.buffers=new PutArrayBuffer[]{buffer1,buffer2,buffer3};
+    //         //////////////////////////////
+    //         PutArrayInput putInput = new PutArrayInput();
+    //         putInput.arrayPath="/array1";
+    //         putInput.arrayType=ArrayType.ARRAY_DENSE;
+    //         putInput.putLayout=LayoutOrder.ROWMAJOR;
+    //         putInput.bufferRange = new long[]{3,6};
+    //         var buffer1 = new PutArrayBuffer("a1",new float[]{1.1f, 2.2f, 3.3f, 4.4f});
+    //         var buffer2 = new PutArrayBuffer("a2", new int[]{9,8,7,5});
+    //         var buffer3 = new PutArrayBuffer("a3", "thisisthestring");
+    //         buffer3.offsets = new long[]{0,4,6,9};
+    //         putInput.buffers=new PutArrayBuffer[]{buffer1,buffer2,buffer3};
 
-            try{
-                store.putArray(putInput);
-            } catch(Exception ex){
-                ex.printStackTrace();
-            }
+    //         try{
+    //             store.putArray(putInput);
+    //         } catch(Exception ex){
+    //             ex.printStackTrace();
+    //         }
 
-            //////////////////////////////
-            GetArrayInput getInput = new GetArrayInput();
-            getInput.arrayPath="/array1";
-            getInput.searchOrder=LayoutOrder.ROWMAJOR;
-            getInput.bufferRange = new long[]{1,10};
-            getInput.attrs = new String[]{"a1","a2","a3"};
+    //         //////////////////////////////
+    //         GetArrayInput getInput = new GetArrayInput();
+    //         getInput.arrayPath="/array1";
+    //         getInput.searchOrder=LayoutOrder.ROWMAJOR;
+    //         getInput.bufferRange = new long[]{1,10};
+    //         getInput.attrs = new String[]{"a1","a2","a3"};
 
-            try{
-                var result = store.getArray(getInput);
-                System.out.println(result);
-            } catch(Exception ex){
-                ex.printStackTrace();
-            }            
+    //         try{
+    //             var result = store.getArray(getInput);
+    //             System.out.println(result);
+    //         } catch(Exception ex){
+    //             ex.printStackTrace();
+    //         }            
 
-        }
+    //     }
 
 
         
 
-    }
+    // }
 
 
 
-    public static void printEnv(){
-        var env = System.getenv();
-        for (Map.Entry<String, String> entry : env.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            System.out.println(String.format("%s:%s",key,value));
-        }
-    }
+    // public static void printEnv(){
+    //     var env = System.getenv();
+    //     for (Map.Entry<String, String> entry : env.entrySet()) {
+    //         String key = entry.getKey();
+    //         String value = entry.getValue();
+    //         System.out.println(String.format("%s:%s",key,value));
+    //     }
+    // }
 }
